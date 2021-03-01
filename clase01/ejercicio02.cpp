14d77f59b903dc1cf86ad512bd01f2300fe94581
Matías J. Rodriguez <mellarodriguez29@gmail.com>
2021-03-01 09:53:15 -0300

    first commit

---
+++ clase01/ejercicio02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    float diagonal1, diagonal2, area;
+    cout << "Ingrese la primer diagonal del rombo" << endl;
+    cin >> diagonal1;
+    cout << "Ingrese la segunda diagonal" << endl;
+    cin >> diagonal2;
+    area = (diagonal1 * diagonal2)/2;
+    cout << "El area del rombo es " << area;
+
+    return 0;
+}
